abcbdac84499bc11f4d7a2ac64bf42a7fdc08359
Shadirayan <33546082+Shadirayan@users.noreply.github.com>
2020-01-25 20:20:47 +0200

    changed and sorted the code and put it into a seperate service

--- Server/API/controllers.js
+++ Server/API/controllers.js
@@ -1,6 +1,5 @@
-var request = require('request');
 const productsFunctions = require("../service/products");
-require('dotenv').config()
+const payWithPaypal = require("../service/paypal")
 
 const controllers = {
   getProductsFromArrayOfIds: ({ body: arrayOfIds }, res) => {
@@ -9,40 +8,11 @@ const controllers = {
   getProducts: (req, res) => {
     res.send(productsFunctions.getProducts());
   },
-  Pay: (req, res) => {
-    var orderId = req.body.orderID;
-    var payerID = req.body.payerID;
-    var totalPrice = req.body.total;
-    request.post(`${process.env.paypal_api}checkout/orders/${orderId}/capture`,
-      {
-        auth:
-        {
-          user: process.env.ID_KEY,
-          pass: process.env.SECRET_KEY
-        },
-        body:
-        {
-          payer_id: payerID,
-          transactions: [
-            {
-              amount:
-              {
-                total: totalPrice,
-                currency: 'USD'
-              }
-            }]
-        },
-        json: true
-      },
-      function (err, response) {
-        if (err) {
-          console.error(err);
-          return res.sendStatus(500);
-        }
-        res.send(
-          response);
-      });
-
+  pay: ({ body: { total, payerID, orderID } }, res) => {
+    payWithPaypal(orderID, payerID, total, (err, response) => {
+      if (err) res.sendstatus(500)
+      else res.send(response)
+    })
   }
 };
 
--- Server/API/router.js
+++ Server/API/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("./controllers");
 
 router.get("/getproducts", controller.getProducts);
-router.post("/pay", controller.Pay);
+router.post("/pay", controller.pay);
 router.post("/getProductsFromArrayOfIds", controller.getProductsFromArrayOfIds);
 
 module.exports = router;
